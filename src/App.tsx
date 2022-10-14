8731d569e09b0bb08079036cc7dfbaac43f3a699
Phuong Pham <phuongphambku@gmail.com>
2022-10-14 09:54:10 +0700

    add LibreFranklin font configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { useEffect } from 'react';
+import { SystemBars } from 'react-native-bars';
 import { hide } from 'react-native-bootsplash';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const App = () => {
   useEffect(() => {
     hide({ fade: true });
   }, []);
 
-  return null;
+  return (
+    <SafeAreaProvider>
+      <SystemBars barStyle="dark-content" animated />
+    </SafeAreaProvider>
+  );
 };
 
 export default App;
