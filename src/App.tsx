422d4e6f8bace704285a8136961a948b4472913e
Phuong Pham <phuongphambku@gmail.com>
2022-10-14 12:17:05 +0700

    add react-native-paper

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { AppStack } from '@rnbd/navigation';
 import { useEffect } from 'react';
 import { SystemBars } from 'react-native-bars';
 import { hide } from 'react-native-bootsplash';
+import { Provider as PaperProvider } from 'react-native-paper';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const App = () => {
@@ -11,12 +12,14 @@ const App = () => {
   }, []);
 
   return (
-    <SafeAreaProvider>
-      <SystemBars barStyle="dark-content" animated />
-      <NavigationContainer>
-        <AppStack />
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <PaperProvider>
+      <SafeAreaProvider>
+        <SystemBars barStyle="dark-content" animated />
+        <NavigationContainer>
+          <AppStack />
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </PaperProvider>
   );
 };
 
